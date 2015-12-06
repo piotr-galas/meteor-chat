e3442309dcec659e1b70c1460b7410f7dc892a16
Piotr <galaspiotrek@gmail.com>
2015-12-06 16:48:40 +0100

    update to view model

--- code/client/messages/messages.js
+++ code/client/messages/messages.js
@@ -1,21 +1,11 @@
-function messages() {
-  return Messages.find({}, { sort: { 'time': -1 } });
-}
-
-function time() {
-  return moment(this.time).format("HH:mm:ss");
-}
-
-function isOwner() {
-  return Meteor.userId() === this.owner;
-}
-
 Template.messages.helpers({
-  messages: messages
+  messages: function(){
+  	return Messages.find({}, { sort: { 'time': -1 } });
+  }
 });
 
 Template.message.helpers({
-  timeFormat: time,
-  isOwner: isOwner,
-
+  messageViewModel: function(){
+  	return new MessageViewModel(this);
+  }	
 });
